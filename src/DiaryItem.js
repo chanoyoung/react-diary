81f81e87d10d19a5709243a8d6b1784e04e2df62
chanoyoung <diruo0725@gmail.com>
2022-05-27 19:16:40 +0900

    Optimization DiaryItem rerender

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import React, { useState, useRef } from "react";
 
 const DiaryItem = ({
   id,
@@ -79,4 +79,4 @@ const DiaryItem = ({
   );
 };
 
-export default DiaryItem;
+export default React.memo(DiaryItem);
